e9d5659f6a104cb80c35adf17817e169538e2725
Glen Cooney <glen@glencooney.com>
2018-01-14 19:55:38 +0000

    Start of card render with hand rotate

---
+++ dragon-drop/src/components/card.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import '../stylesheets/card.css';
+
+const debugStyle = {
+  // visibility: 'hidden'
+}
+
+const originX = 50;
+const originY = 200;
+const rotateDeg = -10;
+
+const card = {
+  marginLeft: `200px`,
+  width: '100px',
+  height: '150px',
+  border: '5px solid black',
+  backgroundColor: 'blue',
+  borderRadius: '8px',
+  display: 'inline-block',
+  transformOrigin: `${originX}% ${originY}%`,
+  msTransformOrigin:`${originX}% ${originY}%`,
+  webkitTransformOrigin:`${originX}% ${originY}%`
+}
+
+const rotate = {
+  transform: `rotate(${rotateDeg}deg)`,
+  msTransform: `rotate(${rotateDeg}deg)`,
+  webkitTransform: `rotate(${rotateDeg}deg)`
+}
+
+class Card extends Component {
+  constructor(props){
+    super(props);
+    
+    this.handleClick = this.handleClick.bind(this);
+    
+    this.state = {
+      cardStyles: card,
+      toggleSpin: false
+    }
+  }
+  
+  handleClick(){
+    let toggle = (this.state.toggleSpin ? false : true);
+    let styles = {};
+    
+    if(toggle){
+      styles = Object.assign({}, card, rotate);
+    }else{
+      styles = card
+    }
+    
+    console.log(`Styles set to ${JSON.stringify(styles)}`);
+    
+    this.setState({
+      toggleSpin: toggle,
+      cardStyles: styles
+    });
+  }
+  
+  render() {
+    return (
+      <div className="Card">
+        <div style={this.state.cardStyles} onClick={this.handleClick}></div>
+        <p style={debugStyle}>Card component loaded</p>
+      </div>
+    );
+  }
+}
+
+export default Card;
