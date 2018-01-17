59daa55a2a4ec3ada8aecb3cc7f525ee9b229119
Glen Cooney <glen@glencooney.com>
2018-01-16 22:41:09 -0800

    Rendering hand WIP. Card highlighting

--- dragon-drop/src/components/card.jsx
+++ dragon-drop/src/components/card.jsx
@@ -5,67 +5,106 @@ const debugStyle = {
   // visibility: 'hidden'
 }
 
-const originX = 50;
-const originY = 200;
-// const rotateDeg = -10;
-
-const card = {
-  marginLeft: `200px`,
-  width: '100px',
-  height: '150px',
-  border: '5px solid black',
-  backgroundColor: 'blue',
-  borderRadius: '8px',
-  display: 'inline-block',
-  transformOrigin: `${originX}% ${originY}%`,
-  msTransformOrigin:`${originX}% ${originY}%`,
-  webkitTransformOrigin:`${originX}% ${originY}%`
-}
-
+// const card = {
+//   marginLeft: `200px`,
+//   width: '100px',
+//   height: '150px',
+//   border: '5px solid black',
+//   backgroundColor: 'blue',
+//   borderRadius: '8px',
+//   display: 'inline-block',
+//   // transformOrigin: `${this.props.originX}% ${this.props.originY}%`,
+//   // msTransformOrigin:`${this.props.originX}% ${this.props.originY}%`,
+//   // webkitTransformOrigin:`${this.props.originX}% ${this.props.originY}%`
+// }
+//
 // const rotate = {
-//   transform: `rotate(${rotateDeg}deg)`,
-//   msTransform: `rotate(${rotateDeg}deg)`,
-//   webkitTransform: `rotate(${rotateDeg}deg)`
+//   // transform: `rotate(${this.props.angle}deg)`,
+//   // msTransform: `rotate(${this.props.angle}deg)`,
+//   // webkitTransform: `rotate(${this.props.angle}deg)`
 // }
 
+// const originX = 50;
+// const originY = 200;
+// const rotateDeg = -10;
 class Card extends Component {
   constructor(props){
     super(props);
-    
+
     this.handleClick = this.handleClick.bind(this);
-    
+    this.toggleHover = this.toggleHover.bind(this);
+
     this.state = {
-      cardStyles: card,
+      // cardStyles: this.getCardStyle(),
+      hover: false,
       toggleSpin: false,
-      originX: this.props.originX,
-      originY: this.props.originY,
-      angle: this.props.angle
+      cardStyles: Object.assign({}, this.getCardStyle(), this.getTransform())
     }
   }
-  
+
+  toggleHover(){
+    this.setState({
+      hover: !this.state.hover,
+      cardStyles: Object.assign({}, this.getCardStyle(), this.getTransform())
+    });
+  };
+
+  getCardStyle(){
+    var borderColor = 'black';
+
+    if(this.state){
+      if(!this.state.hover){
+        borderColor = 'yellow';
+      }
+    }
+
+    return {
+      // marginLeft: `200px`,
+      width: '100px',
+      height: '150px',
+      border: `5px solid ${borderColor}`,
+      backgroundColor: 'blue',
+      borderRadius: '8px',
+      float: `left`,
+      position: 'absolute',
+      transformOrigin: `${this.props.originX}% ${this.props.originY}%`,
+      msTransformOrigin:`${this.props.originX}% ${this.props.originY}%`,
+      WebkitTransformOrigin:`${this.props.originX}% ${this.props.originY}%`
+    }
+  }
+
+  getTransform(){
+    return {
+      transform: `rotate(${this.props.angle}deg)`,
+      msTransform: `rotate(${this.props.angle}deg)`,
+      WebkitTransform: `rotate(${this.props.angle}deg)`
+    }
+  }
+
   handleClick(){
     let toggle = (this.state.toggleSpin ? false : true);
     let styles = {};
-    
+
     if(toggle){
-      styles = Object.assign({}, card, rotate);
+      styles = Object.assign({}, this.getCardStyle(), this.getTransform());
     }else{
-      styles = card
+      styles = this.getCardStyle();
     }
-    
-    console.log(`Styles set to ${JSON.stringify(styles)}`);
-    
+
     this.setState({
       toggleSpin: toggle,
       cardStyles: styles
     });
   }
-  
+
   render() {
     return (
       <div className="Card">
-        <div style={this.state.cardStyles} onClick={this.handleClick}></div>
-        <p style={debugStyle}>Card component loaded</p>
+        <div style={this.state.cardStyles}
+             onClick={this.handleClick}
+             onMouseEnter={this.toggleHover}
+             onMouseLeave={this.toggleHover}>
+        </div>
       </div>
     );
   }
--- dragon-drop/src/components/hand.jsx
+++ dragon-drop/src/components/hand.jsx
@@ -1,40 +1,43 @@
 import React, { Component } from 'react';
-import Card from './components/card.jsx'
+import Card from './card.jsx'
 import '../stylesheets/card.css';
+import _ from 'lodash';
 
 const originX = 50;
 const originY = 200;
 const spreadAngle = 5;
-const handSize = 5;
+const handSize = 10;
 
 class Hand extends Component {
   constructor(props){
     super(props);
-    
+
     this.handleClick = this.handleClick.bind(this);
-    
+
     this.state = {
       toggleFan: false
     }
   }
-  
+
   handleClick(){
     console.log(`Fanning cards`);
   }
-  
+
   getNewAngle(){
       this.spreadAngle+=this.spreadAngle
       return spreadAngle;
   }
-  
+
   render() {
+    var i = 0;
+    var cards = _.times(handSize, ()=>{
+      return <Card originX={originX} originY={originY} angle={5*i} key={i++}/>
+    });
+
     return (
       <div className="hand">
         <p>Hand loaded</p>
-        {_.times(handSize,
-            <Card originX={originX} originY={originY} angle={this.getNewAngle()}/>
-        )}
-        
+        {cards}
       </div>
     );
   }
